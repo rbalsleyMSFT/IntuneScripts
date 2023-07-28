bafbac018d398258f616b154b9eb46838486c466
rbalsleyMSFT <richbal@microsoft.com>
2023-07-28 13:43:18 -0700

    updated script names

--- Remediations/FixSynconLogin/Detect_SyncOnLogin.ps1
+++ Remediations/FixSynconLogin/Detect_SyncOnLogin.ps1
@@ -1,25 +1,48 @@
 #Detect if Login Schedule created by enrollment client exists. This is necessary for machines that were imaged with Windows 2004 and are missing this scheduled task. Upgrades to Windows 20H2+ do not restore the task.
-try{
+try {
     #Find EnrollmentID
-    $enrollment = Get-ChildItem -Path HKLM:\Software\Microsoft\Enrollments -Recurse -ErrorAction SilentlyContinue |
-    ForEach-Object {
-        if((Get-ItemProperty -Path $_.PsPath) -match "DeviceEnroller") { 
-            $_.PsPath 
+    # Define the directory path
+    $directoryPath = "C:\Windows\System32\Tasks\Microsoft\Windows\EnterpriseMgmt"
+
+    # Get the list of subdirectories
+    $subDirectories = Get-ChildItem -Path $directoryPath -Directory
+
+    # Initialize the enrollment variable
+    $enrollment = $null
+
+    # Regex pattern to match a GUID
+    $guidPattern = "^[{(]?[0-9A-Fa-f]{8}-([0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12}[)}]?$"
+
+    # Loop through each subdirectory
+    foreach ($dir in $subDirectories) {
+        if ($dir.Name -match $guidPattern) {
+            $enrollment = $dir.Name
+            break
         }
     }
-    $enrollment = $enrollment.Substring($enrollment.IndexOf('\DeviceEnroller')-36, 36)
+
+    # Output the variable to the console
+    if ($null -ne $enrollment) {
+        Write-Output "Enrollment: $enrollment"
+    }
+    else {
+        Write-Output "No GUID formatted directory found."
+    }
     $task = Get-ScheduledTask -TaskName 'Login Schedule created by enrollment client' -ErrorAction SilentlyContinue -TaskPath "\Microsoft\Windows\EnterpriseMgmt\$enrollment\"
-        If($null -eq $task){
-            Write-Host 'No task'
-            exit 1
-        }
-        else {
-            Write-Host 'Found task'
-            exit 0
-        }
+    If ($null -eq $task) {
+        Write-Host 'No task'
+        exit 1
+    }
+    else {
+        Write-Host 'Found task'
+        exit 0
+    }
 }  
-catch{
+catch {
     $errMsg = $_.Exception.Message
     Write-Error $errMsg
     exit 1
 }
+
+
+
--- Remediations/FixSynconLogin/Remediate_SyncOnLoginScheduledTask.ps1
+++ Remediations/FixSynconLogin/Remediate_SyncOnLoginScheduledTask.ps1
@@ -1,12 +1,33 @@
-try{
+try {
     #Find EnrollmentID
-    $enrollment = Get-ChildItem -Path HKLM:\Software\Microsoft\Enrollments -Recurse -ErrorAction SilentlyContinue |
-    ForEach-Object {
-        if((Get-ItemProperty -Path $_.PsPath) -match "DeviceEnroller") { 
-            $_.PsPath 
+    
+    # Define the directory path
+    $directoryPath = "C:\Windows\System32\Tasks\Microsoft\Windows\EnterpriseMgmt"
+
+    # Get the list of subdirectories
+    $subDirectories = Get-ChildItem -Path $directoryPath -Directory
+
+    # Initialize the enrollment variable
+    $enrollment = $null
+
+    # Regex pattern to match a GUID
+    $guidPattern = "^[{(]?[0-9A-Fa-f]{8}-([0-9A-Fa-f]{4}-){3}[0-9A-Fa-f]{12}[)}]?$"
+
+    # Loop through each subdirectory
+    foreach ($dir in $subDirectories) {
+        if ($dir.Name -match $guidPattern) {
+            $enrollment = $dir.Name
+            break
         }
     }
-    $enrollment = $enrollment.Substring($enrollment.IndexOf('\DeviceEnroller')-36, 36)
+
+    # Output the variable to the console
+    if ($null -ne $enrollment) {
+        Write-Output "Enrollment: $enrollment"
+    }
+    else {
+        Write-Output "No GUID formatted directory found."
+    }
     #21H1 builds have /c /lf switches - 2004 fails when using /lf. /c seems to work fine
     $ST_A = New-ScheduledTaskAction -Execute "%windir%\system32\deviceenroller.exe" -Argument "/o $enrollment /c"
     $ST_T = New-ScheduledTaskTrigger -AtLogOn
@@ -15,7 +36,7 @@ try{
     Register-ScheduledTask -TaskName "Login Schedule created by enrollment client" -Action $ST_A -Trigger $ST_T -Settings $ST_S -Principal $ST_P -TaskPath "\Microsoft\Windows\EnterpriseMgmt\$enrollment\"
     exit 0
 }
-catch{
+catch {
     $errMsg = $_.Exception.Message
     Write-Error $errMsg
     exit 1
